bc716e8b25d83135707ecf42c50fe88c38622913
Carolina de la Fuente <mdela160@fiu.edu>
2017-11-28 00:25:08 -0500

    drums and keys working

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-window.addEventListener('keydown', function(e) {
-  console.log(document.querySelector(`audio[data-key="${e.keyCode}"]`));
-})
+window.addEventListener('keydown', playSound);
+
+function playSound(e) {
+  let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  let key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+  audio.currentTime = 0 //this rewinds the audio so it can play immediately after continuously pressed
+  audio.play();
+  key.classList.add('playing');
+};
+
+function removeTransition(e) {
+  if (e.propertyName != 'transform') return;
+  this.classList.remove('playing')
+}
+
+const keys = document.querySelectorAll('.key')
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
